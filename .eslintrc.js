48eaeef1423d99231c8bf499a21b773435b2bb7f
Darek <DRodzewicz@gmail.com>
2020-07-03 01:46:03 +0200

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+    root: true,
+    env: {
+      node: true
+    },
+    extends: [
+      'plugin:vue/essential',
+      '@vue/standard'
+    ],
+    parserOptions: {
+      ecmaVersion: 2020
+    },
+    rules: {
+      'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      "indent": [
+          "error",
+          4
+      ],
+      "linebreak-style": [
+          "error",
+          "windows"
+      ],
+      "quotes": [
+          "error",
+          "double"
+      ],
+      "semi": [
+          "error",
+          "always"
+      ],
+      "brace-style": [
+          "error",
+          "1tbs"
+      ],
+      "no-console": [
+          "off"
+      ],
+      "brace-style": [
+          "error"
+      ]
+    }
+  }
+  
