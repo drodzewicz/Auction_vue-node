35ff88742cea53c53c1153c0fea9b3ee8c33cecb
Darek <DRodzewicz@gmail.com>
2020-07-03 01:56:42 +0200

    linting problem

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,44 +1,37 @@
 module.exports = {
     root: true,
     env: {
-      node: true
+        node: true
     },
     extends: [
-      'plugin:vue/essential',
-      '@vue/standard'
+        "plugin:vue/essential",
+        "@vue/standard"
     ],
     parserOptions: {
-      ecmaVersion: 2020
+        ecmaVersion: 2020
     },
     rules: {
-      'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-      'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-      "indent": [
-          "error",
-          4
-      ],
-      "linebreak-style": [
-          "error",
-          "windows"
-      ],
-      "quotes": [
-          "error",
-          "double"
-      ],
-      "semi": [
-          "error",
-          "always"
-      ],
-      "brace-style": [
-          "error",
-          "1tbs"
-      ],
-      "no-console": [
-          "off"
-      ],
-      "brace-style": [
-          "error"
-      ]
+        "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+        "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+        indent: [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        quotes: [
+            "error",
+            "double"
+        ],
+        semi: [
+            "error",
+            "always"
+        ],
+        "brace-style": [
+            "error",
+            "1tbs"
+        ]
     }
-  }
-  
+};
